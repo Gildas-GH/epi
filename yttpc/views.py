0761cae929746c0529e7e4a115a5d5eea4f5b599
Adam Topel <amtopel@gmail.com>
2017-07-04 17:27:26 -0400

    Fixed MIME Type

--- yttpc/views.py
+++ yttpc/views.py
@@ -45,6 +45,7 @@ def make_feed_from_playlist(request):
     except HTTPError:
         return HttpResponse('Invalid playlist.')
     
+
     channel_id = get_channel_id(playlist_data)
     channel_data = get_channel_data('channel', channel_id)
 
@@ -61,17 +62,14 @@ def render_feed(request, playlist_data, channel_data, podcast_type):
         date_RFC = email.utils.format_datetime(parsed)
         item['snippet']['publishedAt'] = date_RFC
 
-    #Add channel data to playlist data to make single context
+    #Add channel data to video data to make single context
     playlist_data['channel_data'] = channel_data
     
     #Add media type
     playlist_data['podcast_type'] = podcast_type
     
     #Add media extension
-    if podcast_type == 'video':
-        playlist_data['media_extension'] = 'mp4'
-    else:
-        playlist_data['media_extension'] = 'm4a'
+    playlist_data['media_extension'] = 'mp4' if podcast_type == 'video' else 'm4a'
 
     return render(request, 'yttpc/feed.xml', playlist_data)
 
@@ -100,41 +98,28 @@ def download(request, media_type, video_id):
 
 
 def get_channel_data(id_type, id):
-    """Return a dictionary of channel data from a username or channel id."""
+    """Return a dictionary of channel data from a channel id or username."""
 
-    if id_type == 'channel':
-        id_type = 'id'
-    if id_type == 'user':
-        id_type = 'forUsername'
-
-    qs_params = {'key': KEY,
-                id_type: id,
-                'part': 'contentDetails,snippet'}
-
-    data_url = (ENDPOINT + 'channels?' + urlencode(qs_params))
-
-    with urlopen(data_url) as response:
-        channel_data = response.read()
-
-    channel_data = json.loads(channel_data)
+    id_type = 'id' if id_type == 'channel' else 'forUsername'
+    channel_data = yt_api_call('channels', 'contentDetails,snippet', id_type, id)
+    
     return channel_data
+
 
 
 def get_playlist_data(playlist_id):
     """Return a dictionary of playlist data from a playlist id."""
 
-    qs_params = {'key': KEY,
-                 'playlistId': playlist_id,
-                 'part': 'snippet',
-                 'maxResults': 50}
-
-    data_url = (ENDPOINT + 'playlistItems?' + urlencode(qs_params))
-
-    with urlopen(data_url) as response:
-        json_data = response.read()
-
-    playlist_data = json.loads(json_data)
+    playlist_data = yt_api_call('playlistItems', 'snippet', 'playlistId', playlist_id)
     return playlist_data
+
+
+# def get_videos_data(playlist_data):
+
+#     video_ids = [item['snippet']['resourceId']['videoId'] for item in playlist_data['items']]
+
+#     videos_data = yt_api_call('videos', 'snippet,contentDetails', 'id', ','.join(video_ids))
+#     return videos_data
 
 
 def get_uploads_playlist_id(channel_data):
@@ -149,4 +134,24 @@ def get_channel_id(playlist_data):
  
     channel_id = playlist_data['items'][0]['snippet']['channelId']
     return channel_id
+
+
+
+
+
+def yt_api_call(path, part, id_type, id):
+
+    params = {'key': KEY,
+              id_type: id,
+              'part': part,
+              'maxResults': 50}
+
+    data_url = (ENDPOINT + path + '?' + urlencode(params))
+
+    with urlopen(data_url) as response:
+        json_data = response.read()
+
+    return json.loads(json_data)
+
+
 

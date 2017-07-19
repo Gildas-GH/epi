45e3f4295b957a513cc0cd50d4e19cc1d21c7e63
epiapp <amtopel@outlook.com>
2017-07-19 19:02:47 -0400

    Remove unrepeated functions

--- epi/views.py
+++ epi/views.py
@@ -27,11 +27,11 @@ def index(request):
 
 def make_feed_from_channel(request, id_type, id):
     """Create an RSS feed from a username or channel ID"""
-    
+
     channel_data = get_channel_data(id_type, id)
-    
+
     try:
-        uploads_playlist_id = get_uploads_playlist_id(channel_data)
+        uploads_playlist_id = channel_data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
     except IndexError as e:
         return HttpResponse('It appears that "{}" is not a valid {}.'.format(id, id_type))
 
@@ -47,13 +47,13 @@ def make_feed_from_playlist(request):
         playlist_id = request.GET['list']
     except KeyError:
         return HttpResponse('Invalid URL.')
-    
+
     try:
         playlist_data = get_playlist_data(playlist_id)
     except HTTPError:
         return HttpResponse('It appears that "{}" is not a valid playlist.'.format(request.GET['list']))
-    
-    channel_id = get_channel_id(playlist_data)
+
+    channel_id = playlist_data['items'][0]['snippet']['channelId']
     channel_data = get_channel_data('channel', channel_id)
 
     return render_feed(request, playlist_data, channel_data)
@@ -62,8 +62,9 @@ def make_feed_from_playlist(request):
 def render_feed(request, playlist_data, channel_data):
     """Render the RSS feed from the playlist data, channel data, and desired podcast type (audio or video)."""
 
-    videos_data = get_videos_data(playlist_data)
-    
+    video_ids = [item['snippet']['resourceId']['videoId'] for item in playlist_data['items']]
+    videos_data = yt_api_call('videos', 'snippet,contentDetails', 'id', ','.join(video_ids))
+
     for item in videos_data['items']:
         #Reformat dates for RSS (RFC 2822)
         date_ISO = item['snippet']['publishedAt']
@@ -80,11 +81,11 @@ def render_feed(request, playlist_data, channel_data):
 
     #Add channel data to video data to make single context
     videos_data['channel_data'] = channel_data
-    
+
     #Add media type
     podcast_type = 'video' if int(request.GET.get('vid', 0)) else 'audio'
     videos_data['podcast_type'] = podcast_type
-    
+
     #Add media extension
     videos_data['media_extension'] = 'mp4' if podcast_type == 'video' else 'm4a'
 
@@ -112,7 +113,7 @@ def download(request, media_type, video_id):
     video = pafy.new(video_url)
 
     stream = video.getbest(preftype="mp4") if media_type == 'video' else video.getbestaudio(preftype="m4a")
-    
+
     redirect_url = stream.url
     return redirect(redirect_url)
 
@@ -122,7 +123,7 @@ def get_channel_data(id_type, id):
 
     id_type = 'id' if id_type == 'channel' else 'forUsername'
     channel_data = yt_api_call('channels', 'contentDetails,snippet', id_type, id)
-    
+
     return channel_data
 
 
@@ -131,29 +132,6 @@ def get_playlist_data(playlist_id):
 
     playlist_data = yt_api_call('playlistItems', 'snippet', 'playlistId', playlist_id)
     return playlist_data
-
-
-def get_videos_data(playlist_data):
-    """Return a dictionary of videos data from a set of playlist data."""
-
-    video_ids = [item['snippet']['resourceId']['videoId'] for item in playlist_data['items']]
-
-    videos_data = yt_api_call('videos', 'snippet,contentDetails', 'id', ','.join(video_ids))
-    return videos_data
-
-
-def get_uploads_playlist_id(channel_data):
-    """Extract the 'uploads' playlist id from a channel."""
-
-    playlist_id = channel_data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-    return playlist_id
-
-
-def get_channel_id(playlist_data):
-    """Extract the channel id from a playlist."""
- 
-    channel_id = playlist_data['items'][0]['snippet']['channelId']
-    return channel_id
 
 
 def yt_api_call(path, part, id_type, id):
